6d18a75e0597968872fcc3fee1250894d3845236
rahulsingh2312 <rahulsinghhh2312@gmail.com>
2023-10-07 23:11:02 +0530

    server

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -7,10 +7,10 @@ const cors = require('cors');
 
 
 // Allow requests from localhost:3000
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'https://random-song-swiftie.netlify.app' }));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://random-song-swiftie.netlify.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
