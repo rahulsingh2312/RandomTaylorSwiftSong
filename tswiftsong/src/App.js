bcab82cdbde30e677ee3ed92fc927f5ea92f9424
kamalika0363 <59131714+doesnots@users.noreply.github.com>
2023-10-07 16:02:58 +0530

    Frontend ✅

--- tswiftsong/src/App.js
+++ tswiftsong/src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import YouTube from 'react-youtube';
+// import YouTube from 'react-youtube';
 import Background from "./components/Background/Background.tsx";
 import ts_bg from "./background/ts_bg.png"
-import ts_folk from "./background/folklore.png"
+// import ts_folk from "./background/folklore.png"
 
 
 function RandomTaylorSwiftSong() {
@@ -69,7 +69,7 @@ function RandomTaylorSwiftSong() {
   return (
     <main>
       <div
-        className="h-screen p-16 bg-cover bg-center overflow-auto font-line"
+        className="h-screen p-8 sm:p-16 bg-cover bg-center overflow-auto font-line"
         style={{
           backgroundImage: `url(${ts_bg})`,
           backgroundRepeat: 'no-repeat',
