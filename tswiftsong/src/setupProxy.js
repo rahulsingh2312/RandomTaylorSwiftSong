e4d13ac74a7b1f3b17d011934a243551ac54a668
Rahul <rahulsinghhh2312@gmail.com>
2023-10-07 22:44:36 +0530

    Update setupProxy.js

--- tswiftsong/src/setupProxy.js
+++ tswiftsong/src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function (app) {
-  app.use(
+module.exports = async function (app) {
+  await app.use(
     '/api', // The path that triggers the proxy
     createProxyMiddleware({
       target: 'https://server-9nb0.onrender.com', // The URL of your Express.js server
