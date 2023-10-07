1681057480786eda27b0cebbf7c551702e550e9d
Rahul <rahulsinghhh2312@gmail.com>
2023-10-07 23:00:36 +0530

    Update server.js CORS

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -3,6 +3,19 @@ const fs = require('fs');
 const app = express();
 const port = 3001; // Choose an appropriate port
 
+const cors = require('cors');
+
+
+// Allow requests from localhost:3000
+app.use(cors({ origin: 'http://localhost:3000' }));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.get('/api/taylordata', (req, res) => {
   try {
     const rawData = fs.readFileSync('taylordata.json', 'utf8');
