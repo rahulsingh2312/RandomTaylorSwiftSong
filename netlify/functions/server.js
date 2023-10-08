8d784824a17840ab8f0d229beabf1f14697af447
Rahul <rahulsinghhh2312@gmail.com>
2023-10-08 23:34:34 +0530

    Update server.js added cors for 3000

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -9,12 +9,24 @@ const cors = require('cors');
 // Allow requests from localhost:3000
 app.use(cors({ origin: 'https://random-song-swiftie.netlify.app' }));
 
+app.use(cors({ origin: 'https://localhost:3000' }));
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://random-song-swiftie.netlify.app');
+
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://localhost:3000');
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 
 app.get('/api/taylordata', (req, res) => {
   try {
