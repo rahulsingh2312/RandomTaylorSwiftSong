575df55f780f66bffa572996cf3c22e9f80fb08c
Rahul <rahulsinghhh2312@gmail.com>
2023-10-10 12:25:43 +0530

    Update setupProxy.js for local env

--- tswiftsong/src/setupProxy.js
+++ tswiftsong/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = async function (app) {
   await app.use(
     '/api', // The path that triggers the proxy
     createProxyMiddleware({
-      target: 'http://localhost:3000/', // The URL of your Express.js server
+      target: 'http://localhost:3001/', // The URL of your Express.js server
       changeOrigin: true, // Set this to true if you are using a different origin
     })
   );
