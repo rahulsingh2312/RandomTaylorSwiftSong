f1ad8dc973774823032a448d3547400dc463fedb
Rahul <rahulsinghhh2312@gmail.com>
2023-10-10 12:25:16 +0530

    Update server.js for local env

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -7,12 +7,12 @@ const cors = require('cors');
 
 
 // Allow requests from localhost:3000
-app.use(cors({ origin: 'https://random-song-swiftie.netlify.app' }));
+app.use(cors({ origin: 'http://localhost:3000/' }));
 
 
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://random-song-swiftie.netlify.app');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/');
 
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
