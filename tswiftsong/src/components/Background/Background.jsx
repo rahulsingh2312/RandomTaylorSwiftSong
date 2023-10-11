e2d5210820b2adaf1f3972330084db15f79ec8b7
rizzabh <pandeyrishabh966@gmail.com>
2023-10-11 23:41:09 +0530

    Added Kanye

--- tswiftsong/src/components/Background/Background.jsx
+++ tswiftsong/src/components/Background/Background.jsx
@@ -1,4 +1,4 @@
-import ts from "../../background/ts_1989.svg";
+// import ts from "../../background/ts_1989.svg";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import YouTube from "react-youtube";
@@ -11,6 +11,10 @@ export default function RandomTaylorSwiftSong() {
   const toggle = () => {
     setArtist(artist === "Taylor Swift" ? "Kanye West" : "Taylor Swift");
   };
+  const ts = "/Taylor Swift.png";
+  const kw = `/Kanye West.png`;
+  const ts_bg = "/ts_bg.png";
+  const kw_bg = "/kw_bg.png";
 
   const getRandomSong = async () => {
     //   console.log('API Key:', process.env.REACT_APP_RAPIDAPI_KEY);
@@ -97,43 +101,59 @@ export default function RandomTaylorSwiftSong() {
 
   return (
     <div
-      className="flex justify-center items-center rounded-xl border-2 border-[#2f6e8b] h-full w-full p-8"
+      className=" h-screen p-8 sm:p-16 bg-cover bg-center overflow-auto font-line"
       style={{
-        fill: "linear-gradient(92deg, rgba(255, 255, 255, 0.25) -43.85%, rgba(217, 217, 217, 0.10) 55.97%)",
-        strokeWidth: "1px",
-        stroke: "rgba(255, 255, 255, 0.60)",
-        backdropFilter: "blur(16px)",
+        backgroundImage: `url(${artist == "Taylor Swift" ? ts_bg : kw_bg})`,
+        backgroundRepeat: "no-repeat",
       }}
     >
-      <div className=""></div>
-      <div className="flex flex-col items-center justify-center gap-6">
-        <div>
-          <button
-            onClick={toggle}
-            className="text-white mx-20 mb-10 bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-pink-500 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            {artist}
-          </button>
-          <div className="text-2xl sm:text-4xl font-bold text-pink-400 /90 text-center">
-            {randomSong}
+      <div
+        className="flex justify-center items-center rounded-xl border border-[#737373] h-full w-full p-8"
+        style={{
+          fill: "linear-gradient(92deg, rgba(255, 255, 255, 0.25) -43.85%, rgba(217, 217, 217, 0.10) 55.97%)",
+          strokeWidth: "1px",
+          stroke: "rgba(255, 255, 255, 0.60)",
+          backdropFilter: "blur(6px)",
+        }}
+      >
+        <div className=""></div>
+        <div className="flex flex-col items-center justify-center gap-6">
+          <div>
+            <button
+              onClick={toggle}
+              className="transition ease-in-out duration-700 text-white mx-20 mb-10 bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-gray-900 dark:focus:ring-blue-800"
+            >
+              {artist}{" "}
+              <div>
+                <img src="/down.svg" alt=">" className="ml-2" />
+              </div>
+            </button>
+            <div className="text-2xl sm:text-4xl font-bold text-pink-400 /90 text-center">
+              {randomSong}
+            </div>
           </div>
-        </div>
-        {!videoId && (
-          <img src={ts} alt="description" height={190} width={190} />
-        )}
-        {/*use api to fetch the album images*/}
-        {/* <img src={ts} alt="description"
+          {!videoId && (
+            <img
+              src={artist === "Taylor Swift" ? ts : kw}
+              alt="Artist"
+              height={190}
+              width={190}
+            />
+          )}
+          {/*use api to fetch the album images*/}
+          {/* <img src={ts} alt="description"
                      height={190}
                      width={190}
                 /> */}
-        {videoId && <YouTube videoId={videoId} opts={opts} />}
-        <button
-          onClick={getRandomSong}
-          className="bg-gradient-to-r from-[#1f5175] to-[#00344B]/80 border-2 border-[#2f6e8b]
+          {videoId && <YouTube videoId={videoId} opts={opts} />}
+          <button
+            onClick={getRandomSong}
+            className="transition ease-in duration-200 bg-gradient-to-r from-[#1f5175] to-[#00344B]/80 border-2 border-[#2f6e8b]
                  text-[#DFE6E8] font-semibold rounded-md px-2 sm:px-4 sm:py-2 py-1 text-sm sm:text-xl hover:bg-[#00344B] hover:scale-110"
-        >
-          Get a Random Song
-        </button>
+          >
+            Get a Random Song
+          </button>
+        </div>
       </div>
     </div>
   );
